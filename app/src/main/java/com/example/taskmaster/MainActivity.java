119641dc593e66ddee3a619f4a3ef9a662b58fc2
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-10-31 19:30:42 +0200

    lab-26

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.addTask);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+    @Override
+            public void onClick(View view){
+        Intent intent1 = new Intent(MainActivity.this, Page2.class);
+        startActivity(intent1);
     }
+
+        });
+
+        Button button2Page1 = findViewById(R.id.button3);
+        button2Page1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent2 = new Intent(MainActivity.this, Page3.class);
+                startActivity(intent2);
+            }
+
+        });
+
+
+
+
+    }
+
+
+
+
 }
