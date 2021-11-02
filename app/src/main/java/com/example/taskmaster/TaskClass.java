32c27348e7668987c8e1f365fd74f9db37f9ee6c
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-11-02 20:28:48 +0200

    lab28

---
+++ app/src/main/java/com/example/taskmaster/TaskClass.java
@@ -0,0 +1,15 @@
+package com.example.taskmaster;
+
+public class TaskClass {
+
+    public String title;
+    public String body;
+    public String state;
+
+
+    public TaskClass(String title, String body, String state) {
+        this.title = title;
+        this.body = body;
+        this.state = state;
+    }
+}
